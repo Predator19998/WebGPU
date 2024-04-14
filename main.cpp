9ecc8a6b00a92de95ce2920f8a6300412905035b
Jerry Sivaram Reddy Rajasimha Reddy <raja.simha0@gmail.com>
2024-04-14 10:39:18 -0700

    Changes to shader

--- main.cpp
+++ main.cpp
@@ -30,7 +30,7 @@ void GetDevice(void (*callback)(wgpu::Device)) {
                         const char* message, void* userdata) {
                         UNUSED(status);
                         UNUSED(message);
-                            wgpu::Device device = wgpu::Device::Acquire(cDevice);
+                            device = wgpu::Device::Acquire(cDevice);
                             device.SetUncapturedErrorCallback(
                                 [](WGPUErrorType type, const char* message, void* userdata) {
                                     UNUSED(userdata);
@@ -57,13 +57,22 @@ void SetupSwapChain(wgpu::Surface surface) {
 }
 
 const char shaderCode[] = R"(
-    @vertex fn vertexMain(@builtin(vertex_index) i : u32) ->
-      @builtin(position) vec4f {
-        const pos = array(vec2f(0, 1), vec2f(-1, -1), vec2f(1, -1));
-        return vec4f(pos[i], 0, 1);
+    @vertex
+    fn vs_main(@builtin(vertex_index) in_vertex_index: u32) -> @builtin(position) vec4f {
+        var p = vec2f(0.0, 0.0);
+        if (in_vertex_index == 0u) {
+            p = vec2f(-0.5, -0.5);
+        } else if (in_vertex_index == 1u) {
+            p = vec2f(0.5, -0.5);
+        } else {
+            p = vec2f(0.0, 0.5);
+        }
+        return vec4f(p, 0.0, 1.0);
     }
-    @fragment fn fragmentMain() -> @location(0) vec4f {
-        return vec4f(1, 0, 0, 1);
+
+    @fragment
+    fn fs_main() -> @location(0) vec4f {
+        return vec4f(0.0, 0.4, 1.0, 1.0);
     }
 )";
 
